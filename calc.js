fccd1303c85a1dc9abecb6ad8f1829461c76bbc3
scottyj1234 <scottysoccer30@gmail.com>
2022-04-27 18:35:59 -0500

    Add add function

--- calc.js
+++ calc.js
@@ -17,3 +17,20 @@ function divide(a, b) {
 function operate(operator, a, b) {
     return operator(a, b);
 }
+
+const display = document.querySelector('.display');
+let previousValue = 0;   // stores previous value prior to pressing an operator button
+let currentOperator = null; // stores current operator
+
+document.querySelector('#add').addEventListener('click', (e) => {
+    if (!currentOperator) {
+        currentOperator = add;
+        previousValue = Number.parseFloat(display.textContent);
+    } else {
+        const currentValue = Number.parseFloat(display.textContent);
+        const evaluation = currentOperator(previousValue, currentValue);
+        display.textContent = evaluation.toString();
+        currentOperator = add;
+        previousValue = evaluation;
+    }
+});
