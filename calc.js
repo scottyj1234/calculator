e493712a5ccde58f4933d1b62d0440656dcc3486
scottyj1234 <scottysoccer30@gmail.com>
2022-04-27 20:01:06 -0500

    Implement basic equals functionality

--- calc.js
+++ calc.js
@@ -14,25 +14,26 @@ function divide(a, b) {
     return b === 0 ? "DIVIDE BY 0 ERROR" : a / b;
 }
 
+function equals(currentOperator, b) {
+    return () => currentOperator(b);
+}
+
 function operate(operator, a, b) {
     return operator(a, b);
 }
 
 function operatorClickHandling(operator) {
+    const displayValue = Number.parseFloat(display.textContent);
     if (!currentOperator) {
-        currentOperator = operator;
-        previousValue = Number.parseFloat(display.textContent);
+        currentOperator = (b) => operate(operator, displayValue, b);
     } else {
-        const currentValue = Number.parseFloat(display.textContent);
-        const evaluation = currentOperator(previousValue, currentValue);
+        const evaluation = currentOperator(displayValue);
         display.textContent = evaluation.toString();
-        currentOperator = operator;
-        previousValue = evaluation;
+        currentOperator = (b) => operate(operator, evaluation, b);
     }
 }
 
 const display = document.querySelector('.display');
-let previousValue = 0;   // stores previous value prior to pressing an operator button
 let currentOperator = null; // stores current operator
 
 document.querySelector('#add').addEventListener('click', (e) => {
@@ -49,4 +50,13 @@ document.querySelector('#multiply').addEventListener('click', (e) => {
 
 document.querySelector('#divide').addEventListener('click', (e) => {
     operatorClickHandling(divide);
+});
+
+document.querySelector('#equals').addEventListener('click', (e) => {
+    if (currentOperator){
+        const displayValue = Number.parseFloat(display.textContent);
+        const evaluation = currentOperator(displayValue);
+        display.textContent = evaluation;
+        currentOperator = null;
+    }
 });
