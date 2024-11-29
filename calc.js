6a33a7cdbd04e0fa5e2d46038abd092e4043424a
Scott J <70438931+scottyj1234@users.noreply.github.com.>
2024-11-29 17:08:54 -0600

    Add operation action

--- calc.js
+++ calc.js
@@ -1,3 +1,7 @@
+let leftOperand = null;
+let rightOperand = null;
+let operator = null;
+
 function add(a, b) {
     return a + b;
 }
@@ -12,4 +16,24 @@ function multiply(a, b) {
 
 function divide(a, b) {
     return a / b;
+}
+
+function operate(leftOperand, rightOperand, operator) {
+    switch (operator) {
+        case "+":
+            return add(leftOperand, rightOperand);
+        
+        case "-":
+            return subtract(leftOperand, rightOperand);
+        
+        case "*":
+            return multiply(leftOperand, rightOperand);
+        
+        case "/":
+            return divide(leftOperand, rightOperand);
+    
+        default:
+            console.error(`Critical error: Invalid operator: ${operator}`);
+            return null;
+    }
 }
