c4927f391f41bd3a9bcc0830b0bd5f07098dfd9a
Scott J <70438931+scottyj1234@users.noreply.github.com.>
2024-11-30 19:23:34 -0600

    Add button functions

--- calc.js
+++ calc.js
@@ -1,6 +1,7 @@
 let leftOperand = null;
-let rightOperand = null;
 let operator = null;
+let displayToClear = false;
+let allToClear = false;
 
 function add(a, b) {
     return a + b;
@@ -38,16 +39,48 @@ function operate(leftOperand, rightOperand, operator) {
     }
 }
 
+// starting value for display
 const displayElement = document.querySelector("div.display");
 let displayValue = "0";
 
+// setup numerical buttons
 const numberButtons = document.querySelectorAll("button.numberButton");
 numberButtons.forEach((button) => button.addEventListener("click", (e) => {
     addDigit(button.textContent);
 }))
 
+// setup operator buttons
+const operatorButtons = document.querySelectorAll("button.operatorButton");
+operatorButtons.forEach((button) => button.addEventListener("click", (e) => {
+    operatorEntry(button.textContent);
+}))
+
+// decimal button
+document.querySelector("button#btnDecimal").addEventListener("click", (e) => {
+    addDecimalAttempt();
+})
+
+// clear button
+document.querySelector("button#btnClear").addEventListener("click", (e) => {
+    clearAll();
+})
+
+function clearAll() {
+    leftOperand = null;
+    rightOperand = null;
+    operator = null;
+    allToClear = false;
+    displayToClear = false;
+    displayValue = "0";
+    displayElement.textContent = displayValue;
+}
+
 function addDigit(numberCharacter) {
-    if (displayValue === "0") {
+    // "=" was last pressed and result on screen. any digit pressed after this results in clean slate.
+    if (allToClear) clearAll();
+
+    // if displayToClear is true, then an operator was pressed and we are showing the previous value up until they start entering the next value
+    if (displayValue === "0" || displayToClear) {
         displayValue = numberCharacter;
     } else {
         displayValue += numberCharacter;
@@ -55,3 +88,30 @@ function addDigit(numberCharacter) {
     displayElement.textContent = displayValue;
 }
 
+function operatorEntry(operatorPressed) {
+    if (operatorPressed === '=') {
+        leftOperand = operate(leftOperand, rightOperand, operator);
+        displayValue = String(leftOperand)
+        displayElement.textContent = displayValue;
+        allToClear = true;
+        return;
+    }
+
+    if (leftOperand !== null) {
+        rightOperand = Number(displayValue);
+        leftOperand = operate(leftOperand, rightOperand, operator);
+        operator = operatorPressed;
+        displayValue = String(leftOperand);
+    } else {
+        leftOperand = Number(displayValue);
+        operator = operatorPressed;
+    }
+
+    displayToClear = true;
+}
+
+function addDecimalAttempt() {
+    if (displayValue.includes(".")) return;
+    displayValue += ".";
+    displayElement.textContent = displayValue;
+}
